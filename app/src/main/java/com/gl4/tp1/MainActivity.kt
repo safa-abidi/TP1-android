b2fcc11c53e0d1f7af2867b0cb71af0f07771179
safa-abidi <safa.abidi99@gmail.com>
2022-10-05 08:38:18 +0100

    ex 1

--- app/src/main/java/com/gl4/tp1/MainActivity.kt
+++ app/src/main/java/com/gl4/tp1/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.gl4.tp1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit var txtEmail : EditText
+    lateinit var txtPassword : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         txtEmail = findViewById(R.id.editTextTextEmailAddress)
+        txtPassword = findViewById(R.id.editTextTextPassword)
 
     }
     var email = txtEmail.text.toString()
+    var password = txtPassword.text.toString()
+
+    fun login(v: View?) {
+        if(email == "gl4@insat.tn" && password =="insat2022"){
+            Toast.makeText(this,"login successful",Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this,"wrong credentials",Toast.LENGTH_SHORT).show()
+        }
+    }
 }
